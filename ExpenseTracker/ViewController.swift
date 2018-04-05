aad027c8cc49b49c394188281c8afa12c64fc05e
dennisyd <dennisyd@alum.mit.edu>
2018-04-05 00:24:50 -0400

    loading content on app start and updating tableView; adding TableViewDataSource

--- ExpenseTracker/ViewController.swift
+++ ExpenseTracker/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var shopTableView: UITableView!
+    
+    var receiptStore: Store!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        receiptStore = Store()
+        receiptStore.update(completion: {_ in
+            DispatchQueue.main.async {
+                self.shopTableView.reloadData()
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +33,15 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return receiptStore.shops.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shopCell", for: indexPath) as! ShopCellTableViewCell
+        cell.format(shop: receiptStore.shops[indexPath.row])
+        return cell
+    }
+    
+}
